c5310276e38c1cc8de0e4b94d13ebe03b4aceee3
200517 <200517@ad.meisei-mic.ac.jp>
2021-05-18 12:03:09 +0900

    new v

--- L3SampleP4.java
+++ L3SampleP4.java
@@ -4,13 +4,13 @@ import java.io.*;
 public class L3SampleP4 {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("円銃率の値はいくつですか？");
+        System.out.println("円周率の値はいくつですか？");
         BufferedReader Br = new BufferedReader(new InputStreamReader(System.in));
 
         String data = Br.readLine();
 
         double doubleData = Double.parseDouble(data);
 
-        System.out.println("円銃率の値" + doubleData + "です");
+        System.out.println("円周率の値" + doubleData + "です");
     }
 }
