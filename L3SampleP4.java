5d3f73b457670b34659169afd00c00a94d5b45aa
200517 <200517@ad.meisei-mic.ac.jp>
2021-05-18 11:30:24 +0900

    java input

---
+++ L3SampleP4.java
@@ -0,0 +1,16 @@
+//200517 ダスリドチャンダラ
+import java.io.*;
+
+public class L3SampleP4 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("円銃率の値はいくつですか？");
+        BufferedReader Br = new BufferedReader(new InputStreamReader(System.in));
+
+        String data = Br.readLine();
+
+        double doubleData = Double.parseDouble(data);
+
+        System.out.println("円銃率の値" + doubleData + "です");
+    }
+}
