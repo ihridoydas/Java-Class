e39db9d579bf476876bcaa2cff7d4b9331735ef1
200517 <200517@ad.meisei-mic.ac.jp>
2021-06-08 10:12:07 +0900

    java 6/8

---
+++ L4Sample5.java
@@ -0,0 +1,12 @@
+public class L4Sample5 {
+  public static void main(String[] args) {
+    int a=0;
+    int b=0;
+    b=a++;
+    System.out.println("代入後にインクリメントしたのでbの値は"+b+"です。");
+    a=2<<5;
+    b=50>>2;
+    System.out.println(a);
+    System.out.println(b);
+}
+}
