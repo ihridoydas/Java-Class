356376ad15629e2119995e3e138b241231e0655a
200517 <200517@ad.meisei-mic.ac.jp>
2021-06-08 10:55:22 +0900

    6/8

--- L4Sample9.java
+++ L4Sample9.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
   
     System.out.println("身長は"+ dnum + "センチです");
     System.out.println("int型の変換に代入します");
-    double inum = dnum;
+    int inum = (int)dnum;
     System.out.println("身長は"+ inum + "センチです");
 }
 }
