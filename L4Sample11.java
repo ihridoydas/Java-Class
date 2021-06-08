b0faff22b5ed9f6342d70de426978ac6021693a0
200517 <200517@ad.meisei-mic.ac.jp>
2021-06-08 11:48:58 +0900

    6/8 final

--- L4Sample11.java
+++ L4Sample11.java
@@ -1,10 +1,15 @@
 public class L4Sample11 {
 
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 4;
-        double div = num1 / num2;
+        /*
+         * float num1 = 5; float num2 = 4; float div = num1 / num2;
+         * 
+         * System.out.println("5/4 " + div + "です");
+         */
+        int num3 = 5;
+        int num4 = 4;
+        double div1 = (double) num3 / num4;
 
-        System.out.println("5/4 " + div + "です");
+        System.out.println("5/4 " + div1 + "です");
     }
 }
