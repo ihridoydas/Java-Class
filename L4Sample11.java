6cecfba6212af242eb74c97644c0438f67594ff9
200517 <200517@ad.meisei-mic.ac.jp>
2021-06-08 11:21:12 +0900

    6/8

---
+++ L4Sample11.java
@@ -0,0 +1,10 @@
+public class L4Sample11 {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        double div = num1 / num2;
+
+        System.out.println("5/4 " + div + "です");
+    }
+}
